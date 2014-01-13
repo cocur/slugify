3dec6b10a84928de9e8824a5de317d7748283058
Florian Eckerstorfer <florian@theroadtojoy.at>
2014-01-13 15:47:09 +0100

    Updated PHPDoc

--- src/SlugifyInterface.php
+++ src/SlugifyInterface.php
@@ -1,12 +1,32 @@
 <?php
+
+/**
+ * This file is part of cocur/slugify.
+ *
+ * (c) Florian Eckerstorfer <florian@eckerstorfer.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Cocur\Slugify;
 
+/**
+ * SlugifyInterface
+ *
+ * @package   org.cocur.slugify
+ * @author    Florian Eckerstorfer <florian@eckerstorfer.co>
+ * @author    Marchenko Alexandr
+ * @copyright 2012-2014 Florian Eckerstorfer
+ * @license   http://www.opensource.org/licenses/MIT The MIT License
+ */
 interface SlugifyInterface
 {
     /**
      * Return a URL safe version of a string.
      *
      * @param string $string
+     *
      * @return string
      */
     public function slugify($string);
