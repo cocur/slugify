8349cdcd14b6cde6346b1198736728ca968aa86e
Michal Skop <michal.skop@kohovolit.eu>
2014-08-24 16:07:18 +0200

    change in Czech slug of D/d with caron

--- src/Slugify.php
+++ src/Slugify.php
@@ -306,7 +306,7 @@ class Slugify implements SlugifyInterface
 
         // Czech
         'Č' => 'C',
-        'Ď' => 'Dj',
+        'Ď' => 'D',
         'Ě' => 'E',
         'Ň' => 'N',
         'Ř' => 'R',
