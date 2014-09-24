fc0adb5a4385b94df38160cf78edb0fb69a119bd
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-09-24 11:46:47 +0200

    Added test for the regExp feature

--- tests/SlugifyTest.php
+++ tests/SlugifyTest.php
@@ -110,6 +110,20 @@ class SlugifyTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Cocur\\Slugify\\SlugifyInterface', Slugify::create());
     }
 
+    /**
+     * @test
+     * @covers Cocur\Slugify\Slugify::setRegExp()
+     */
+    public function otherRegExpsProduceOtherResults()
+    {
+        $actual = 'File Name.tar.gz';
+        $expected = 'file-name.tar.gz';
+
+        $this->assertNotEquals($expected, $this->slugify->slugify($actual));
+        $this->slugify->setRegExp('/([^a-z0-9.]|-)+/');
+        $this->assertEquals($expected, $this->slugify->slugify($actual));
+    }
+
     public function provider()
     {
         return array(
