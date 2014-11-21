a92e5b49408aec9da0d3c6240450156f462d1cde
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-11-21 20:04:50 +0100

    Created ZF2 bridge Slugify service

---
+++ src/Bridge/ZF2/Module.php
@@ -0,0 +1,47 @@
+<?php
+namespace Cocur\Slugify\Bridge\ZF2;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+
+/**
+ * Class Module
+ * @package    cocur/slugify
+ * @subpackage bridge
+ * @license    http://www.opensource.org/licenses/MIT The MIT License
+ */
+class Module implements ConfigProviderInterface, ServiceProviderInterface
+{
+    const CONFIG_KEY = 'cocur_slugify';
+
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return array(
+
+        );
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array
+     */
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Cocur\Slugify\Slugify' => 'Cocur\Slugify\Bridge\ZF2\SlugifyService'
+            ),
+            'aliases' => array(
+                'slugify' => 'Cocur\Slugify\Slugify'
+            )
+        );
+    }
+}
+ 
