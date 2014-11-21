db5e4f93fc4cfc31e41ffb356e0665eb0e3a50d4
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-11-21 20:41:37 +0100

    Created ZF2 bridge ModuleTest

---
+++ tests/Bridge/ZF2/ModuleTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Cocur\Slugify\Bridge\ZF2;
+
+/**
+ * Class ModuleTest
+ * @package    cocur/slugify
+ * @subpackage bridge
+ * @license    http://www.opensource.org/licenses/MIT The MIT License
+ */
+class ModuleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Module
+     */
+    private $module;
+
+    public function setUp()
+    {
+        $this->module = new Module();
+    }
+
+    /**
+     * @test
+     * @covers Cocur\Slugify\Bridge\ZF2\Module::getServiceConfig()
+     */
+    public function getServiceConfig()
+    {
+        $smConfig = $this->module->getServiceConfig();
+        $this->assertTrue(is_array($smConfig));
+        $this->assertArrayHasKey('factories', $smConfig);
+        $this->assertArrayHasKey('Cocur\Slugify\Slugify', $smConfig['factories']);
+        $this->assertArrayHasKey('aliases', $smConfig);
+        $this->assertArrayHasKey('slugify', $smConfig['aliases']);
+    }
+
+    /**
+     * @test
+     * @covers Cocur\Slugify\Bridge\ZF2\Module::getViewHelperConfig()
+     */
+    public function getViewHelperConfig()
+    {
+        $vhConfig = $this->module->getViewHelperConfig();
+        $this->assertTrue(is_array($vhConfig));
+        $this->assertArrayHasKey('factories', $vhConfig);
+        $this->assertArrayHasKey('slugify', $vhConfig['factories']);
+    }
+}
